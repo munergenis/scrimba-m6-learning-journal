2803af43553dafed168cd96a51af4e8cea9b00d8
munergenis <154993411+munergenis@users.noreply.github.com>
2024-04-01 15:05:03 +0200

    adds js functionality to index.html

---
+++ src/modules/data.js
@@ -0,0 +1,38 @@
+export const blogData = [
+  {
+    title: "Blog one",
+    date: new Date(2022, 11, 23),
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    img: "/assets/images/blog-one-image.png",
+  },
+  {
+    title: "Blog two",
+    date: new Date(2023, 2, 15),
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    img: "/assets/images/blog-two-image.png",
+  },
+  {
+    title: "Blog three",
+    date: new Date(2020, 0, 3),
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    img: "/assets/images/blog-three-image.png",
+  },
+  {
+    title: "Blog four",
+    date: new Date(2024, 5, 14),
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    img: "/assets/images/blog-four-image.png",
+  },
+  {
+    title: "Blog five",
+    date: new Date(2022, 11, 22),
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    img: "/assets/images/blog-five-image.png",
+  },
+  {
+    title: "Blog six",
+    date: new Date(2023, 9, 17),
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    img: "/assets/images/blog-six-image.png",
+  },
+]
